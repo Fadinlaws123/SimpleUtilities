4f99bfeff271cba9f80eb9e731c71793cf9ec59d
Fadin_laws <144308790+Fadinlaws123@users.noreply.github.com>
2024-01-22 07:51:59 -0600

    Delete config.lua

--- config.lua
+++
@@ -1,7 +0,0 @@
-Config = {}
-
-Config.Scripts = {
-  weaponFlashlight = true, -- Enable / Disable the persistant weapon flashlight.
-  disableKnockout = true, -- Enable / Disable the pistol whipping feature from GTA 5
-  disableCombatRoll = true, -- Enable / Disable people being able to combat roll in a shooting.
-}
